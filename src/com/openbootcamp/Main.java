1edc9ffab2b00a10e269743a2c90022c00e6d589
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2022-09-01 16:53:45 -0300

    Version 1.2

--- src/com/openbootcamp/Main.java
+++ src/com/openbootcamp/Main.java
@@ -1,5 +1,6 @@
 package com.openbootcamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -16,8 +17,16 @@ public class Main {
 
         Stream<String> valores = nombres.stream().map(x -> " HOLA " +  x.toUpperCase());
         valores.forEach(x -> System.out.println(x));
+        
+        int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+        var stNumeros = Arrays.stream(numeros);
+        var resultado = stNumeros.filter(x -> x % 2 ==0 ).reduce( 0 , (x, y) -> {
+            System.out.println("X es: " + x + " , Y es: " + y);
+            return x +y;
+        });
+        System.out.println("Mi suma de pares es: " + resultado);
 
-
+    //resultado.forEach(System.out::println);
 
     }
 }
